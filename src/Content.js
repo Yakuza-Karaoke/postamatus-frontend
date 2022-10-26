6f1ca486d63ac7aab801e6181664029379fbd5b3
Berlimor <eldaryusufovooo@gmail.com>
2022-10-26 21:59:01 +0300

    Finished the layout of the main page. Functionality is in work.

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,23 @@
 import './index.css';
 import MyMap from './components/Map';
+import Button from './components/Button'
+import Footer from './Footer';
 
 function Content () {
     return (
-        <div className='container border'>
+        <div className='container'>
             <div className='row justify-content-center'>
                 <h1 className='col-auto'>Установка постамата в <strong><u>г. Санкт-петербурге</u></strong></h1>
-                <div id='map' className='col-auto me-auto'><MyMap /></div>
+                
+                <div className='row justify-content-around'>
+                    <div className='col-auto'><MyMap /></div>
+                
+                    <div className='col-4'>
+                            <Button name="Добавить постамат" class="button"/>
+                            <Button name="Рассчитать оценку локации" class="button count"/>
+                    </div>
+                </div>
+                <Footer />
             </div>
             
         </div>
