a57fad76e0562df6d755fac13d1207138457fd13
Nikolas <timofeevnik41@gmail.com>
2022-10-26 11:16:18 +0300

    fix: yandex maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,25 @@
-function MyMap() {
-  let map1 = new ymaps.Map('map', {
-    center: [60, 30.4],
-    zoom: 9
-  })
-  return <div></div>
-}
+import { Map, GeoObject } from "@pbe/react-yandex-maps";
 
-ymaps.ready(MyMap)
+function MyMap() {
+  // Документация по яндекс картам: https://pbe-react-yandex-maps.vercel.app/?path=/docs/getting-started--page
+  return (
+    <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }}>
+      <GeoObject
+        geometry={{
+          type: "LineString",
+          coordinates: [
+            [55.76, 37.64],
+            [52.51, 13.38],
+          ],
+        }}
+        options={{
+          geodesic: true,
+          strokeWidth: 5,
+          strokeColor: "#F008",
+        }}
+      />
+    </Map>
+  );
+}
 
 export default MyMap;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Header from './Header';
 import Content from './Content';
+import { YMaps } from '@pbe/react-yandex-maps';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
+    <YMaps>
         <Header />
         <Content />
+    </YMaps>
     </div>
 );
 
