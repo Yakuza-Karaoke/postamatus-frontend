80d38604d40288a1346bb56137f40e857f34abaf
Heslimited <dimzo333@gmail.com>
2022-11-01 19:32:30 +0300

    Бэкграунд 404

--- src/pages/page404.js
+++ src/pages/page404.js
@@ -1,5 +1,12 @@
+import Footer from "../Footer";
+
 export const page404 = (
   <>
+    <Footer />
     <h1>404 not found</h1>
+    <div className='Background'>
+      background: #333; /* Цвет фона */
+    color: #fc0; /* Цвет текста */
+    </div>
   </>
 );
