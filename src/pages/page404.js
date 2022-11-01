61160581f2c429113b6a3ff20dfd715210036924
Heslimited <dimzo333@gmail.com>
2022-11-01 19:56:57 +0300

    Background 404

--- src/pages/page404.js
+++ src/pages/page404.js
@@ -2,11 +2,9 @@ import Footer from "../Footer";
 
 export const page404 = (
   <>
-    <Footer />
     <h1>404 not found</h1>
-    <div className='Background'>
-      background: #333; /* Цвет фона */
-    color: #fc0; /* Цвет текста */
-    </div>
+
+    <Background />
+    <Footer />
   </>
 );
