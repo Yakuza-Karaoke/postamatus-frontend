08ff6f6a40e56cf70fa77d862917a69e84d79e71
Nikolas <timofeevnik41@gmail.com>
2022-11-05 18:46:30 +0300

    clickable maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,41 +1,76 @@
-import { Map, Placemark } from "@pbe/react-yandex-maps";
+// import { Map, Placemark } from "@pbe/react-yandex-maps";
 
-function MyMap() {
-  // Документация по яндекс картам: https://pbe-react-yandex-maps.vercel.app/?path=/docs/getting-started--page
-  return (
-    <Map
-      width={"550px"}
-      height={"500px"}
-      defaultState={{
-        center: [55.755864, 37.617698],
-        zoom: 10,
-        controls: ["zoomControl", "fullscreenControl"],
-      }}
-      modules={["control.ZoomControl", "control.FullscreenControl"]}
-    >
-      <Placemark geometry={[55.834472, 37.658059]} />
-      <Placemark geometry={[55.835488, 37.658347]} />
-      <Placemark geometry={[55.798485, 37.52068]} />
-      <Placemark geometry={[55.723592, 37.527256]} />
-      <Placemark geometry={[55.71518, 37.506612]} />
-      <Placemark geometry={[55.80651, 37.449866]} />
-      <Placemark geometry={[55.802462, 37.591934]} />
-      <Placemark geometry={[55.661333, 37.509352]} />
-      <Placemark geometry={[55.780851, 37.449156]} />
-      <Placemark geometry={[55.803575, 37.591036]} />
-      <Placemark geometry={[55.803003, 37.591503]} />
-      <Placemark geometry={[55.579694, 37.671094]} />
-      <Placemark geometry={[55.774598, 37.54595]} />
-      <Placemark geometry={[55.774249, 37.546543]} />
-      <Placemark geometry={[55.827671, 37.487568]} />
-      <Placemark geometry={[55.876703, 37.484065]} />
-      <Placemark geometry={[55.665477, 37.547674]} />
-      <Placemark geometry={[55.744308, 37.419521]} />
-      <Placemark geometry={[55.774031, 37.467221]} />
-      <Placemark geometry={[55.637398, 37.600397]} />
-      <Placemark geometry={[55.716042, 37.503998]} />
-    </Map>
-  );
+// function MyMap() {
+//   // Документация по яндекс картам: https://pbe-react-yandex-maps.vercel.app/?path=/docs/getting-started--page
+
+//   let picked_point = null;
+
+//   const clickOnMap = (e) => {
+//     console.log(e.get("coords"));
+//     picked_point = e.get("coords");
+//   };
+
+//   return (
+//     <Map
+//       width={"550px"}
+//       height={"500px"}
+//       defaultState={{
+//         center: [55.755864, 37.617698],
+//         zoom: 10,
+//         controls: ["zoomControl", "fullscreenControl"],
+//       }}
+//       modules={["control.ZoomControl", "control.FullscreenControl"]}
+//       onClick={clickOnMap}
+//     >
+//       {picked_point ? <Placemark geometry={picked_point} /> : null}
+//     </Map>
+//   );
+// }
+
+// export default MyMap;
+
+
+import React, { Component } from 'react'
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+
+export default class MyMap extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            coords: [],
+            mapState: {
+                center: [41.2825125, 69.1392826],
+                zoom: 9
+            },
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.oldCoords !== this.props.oldCoords) {
+            this.setState({ coords: this.props.oldCoords })
+        }
+    }
+
+    onMapClick = (e) => {
+        const coords = e.get("coords");
+        this.setState({ coords: coords })
+    };
+
+    render() {
+        return (
+            <div>
+                <YMaps query={{ apikey: "6054728e-f3f0-4a09-b10c-8e6d17c49bf2" }}>
+                    <Map
+                        modules={["Placemark", "geoObject.addon.balloon"]}
+                        onClick={this.onMapClick}
+                        state={this.state.mapState}
+                        width='100%'
+                        height='500px'
+                    >
+                        {this.state.coords ? <Placemark geometry={this.state.coords} /> : null}
+                    </Map>
+                </YMaps>
+            </div>
+        )
+    }
 }
-
-export default MyMap;
