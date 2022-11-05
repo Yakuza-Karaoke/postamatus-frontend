fd55f97276bfa10447f4bc89093ee7a879623e18
Heslimited <dimzo333@gmail.com>
2022-11-05 03:03:33 +0300

    Fix for button area to work correctly Fix for footer(Moving down)

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MainPage from "./pages/mainPage";
 import Page404 from "./pages/page404";
-
+import ChangePass from "./pages/changePass";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render (
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<ChangePass />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/*" element={<Page404 />} />
       </Routes>
