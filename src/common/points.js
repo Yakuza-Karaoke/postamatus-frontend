f91ed6d707397b85ce99e99a0f179c49755d80a3
Nikolas <timofeevnik41@gmail.com>
2022-11-06 17:56:03 +0300

    save postamat logic

--- src/common/points.js
+++ src/common/points.js
@@ -24,6 +24,24 @@ export const doCalcScore = (lat, long) => {
   return promise;
 };
 
+export const doSavePostamat = (lat, long, score) => {
+    return new Promise((resolve, reject) => {
+        axios
+          .post(`http://178.170.192.207:8000/points/postamat?lat=${lat}&long=${long}&score=${score}`, {
+            headers: {
+              "Content-Type": "application/json",
+              token: localStorage.getItem("token"),
+            },
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              resolve(res);
+            } else reject(res);
+          })
+          .catch((error) => reject(error));
+      });
+}
+
 export const savePointCalc = (data) => {
   localStorage.setItem();
 };
--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -4,6 +4,8 @@ import { YMaps } from "@pbe/react-yandex-maps";
 import MyMap from "../components/Map";
 import { Navigate } from "react-router-dom";
 import Button from "../components/Button";
+import { doSavePostamat } from "../common/points";
+import { toast } from "react-toastify";
 
 export default function MainPage() {
   if (localStorage.getItem("authenticated") === "true") {
@@ -12,8 +14,17 @@ export default function MainPage() {
     const onCalculate = (data) => {
       pointCalculations = data.point;
       document.getElementById("score").innerHTML =
-        "Оценка локации: " + data.point.score;
+        "Оценка локации: " + data.point.score.toFixed(2);
+      console.log(pointCalculations.coords[0], pointCalculations.coords[1], pointCalculations.score);
     };
+
+    const onSavePostamat = () => {
+      if (!pointCalculations) {
+        toast.warn('Вы не выбрали точку на карте!')
+        return
+      }
+      doSavePostamat(pointCalculations.coords[0], pointCalculations.coords[1], pointCalculations.score)
+    }
 
     return (
       <YMaps query={{ apikey: "6054728e-f3f0-4a09-b10c-8e6d17c49bf2" }}>
@@ -29,7 +40,7 @@ export default function MainPage() {
 
             <div className="row justify-content-around">
               <div className="col-4">
-                <Button name="Добавить постамат" class="button" />
+                <Button name="Добавить постамат" class="button" onClickHandler={onSavePostamat} />
               </div>
 
               <h3 id="score" className="text-center"></h3>
