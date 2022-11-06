c6cfb446a413f297b7615efdcb278aa4cf5d0e22
Heslimited <dimzo333@gmail.com>
2022-11-06 19:36:34 +0300

    Last changes for profile page.

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -12,40 +12,62 @@ export default function ProfilePage() {
     <div>
       <Header title="Личный кабинет" />
       <div className="text-center mt-5">
-        <div className="text-center">
-          <h3>Последние добавленные точки</h3>
-        </div>
-        <div className="container-fluid">
-          <div className="row justify-content-end">
-          <div className="col text-change-password mb-4">
-          Хотите поменять пароль?
-                      </div>
-            <div className="col-4">
-              <TableForProfile />
-            </div>
-            <div className="col-4">
-              <div className="cointainer-fluid">
-                <div className="row">
-                  <div className="col-5"></div>
-                  <div className="col-4">
-                    <div className="row justify-content-center">
-                      
-                      <div className="col-12 text-center text-change-password mb-4">
-                      Добро пожаловать, путин в.в
-                      </div>
-                      <div className="col-7 text-center">
-                        <a
-                          className="text-center btn btn-light btn-lg  button-above-tcp"
-                          href="/changePassword"
-                        >
-                          <div onClick={Return}>ТЫК</div>
-                        </a>
-                      </div>
+        <h3>Последние добавленные точки</h3>
+      </div>
+      <div className="text-center container-fluid">
+        <div className="row justify-content-end">
+          <div className="col-2 mb-4">
+            <h4>Добро пожаловать, путин в.в</h4>
+          </div>
+          <div className="col-2  mb-4">
+            <h4>ВАШ ID - 2281337</h4>
+          </div>
+          <div className="col-4">
+            <TableForProfile />
+          </div>
+          <div className="col-4">
+            <div className="cointainer-fluid">
+              <div className="row">
+                <div className="col-5"></div>
+                <div className="col-4">
+                  <div className="row justify-content-center">
+                    <div className="col-19 text-center text-change-password mb-4">
+                      <h5>Хотите поменять пароль?</h5>
+                    </div>
+                    <div className="col-7 text-center">
+                      <a
+                        className="text-center btn btn-light btn-lg  button-above-tcp"
+                        href="/changePassword"
+                      >
+                        <div onClick={Return}>ТЫК</div>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-secondary">
+        <div className="row">
+          <div className="text-change-password-statistic-header text-center col-2 mb-4 mt-5">
+            <h2>Ваша статистика:</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="text-change-password-statistic col-2  mb-5">
+            Точек добавлено - 228
+          </div>
+        </div>
+        <div className="row">
+          <div className="text-change-password-statistic col-2 mb-5">
+            Точек установлено - 000
+          </div>
+        </div>
+        <div className="row">
+          <div className="text-change-password-statistic col-2  mb-5">
+            Точек отменено - 666
           </div>
         </div>
       </div>
