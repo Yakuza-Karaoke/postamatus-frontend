69e6dab673a78e29173123a2095a1b6f4e2b6c8a
Berlimor <eldaryusufovooo@gmail.com>
2022-11-06 17:57:04 +0300

    Replaced some content in main page.

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -18,24 +18,38 @@ export default function MainPage() {
     return (
       <YMaps query={{ apikey: "6054728e-f3f0-4a09-b10c-8e6d17c49bf2" }}>
         <Header title="Главная" />
-        <div className="container">
+        <div className="container-xl">
           <div className="row justify-content-center">
-            <p className="col h3 p-3">
+            <p className="col h3 p-3 text-center">
               Установка постамата в{" "}
               <strong>
                 <u>г. Москва</u>
               </strong>
             </p>
 
-            <div className="row justify-content-around">
-              <div className="col-4">
-                <Button name="Добавить постамат" class="button" />
-              </div>
+            <div className="container-fluid">
+              <div className="row justify-content-around">
+                <div className="col-6">
+                  <div className="col-11">
+                    <Button name="Добавить постамат" class="button" />
+                  </div>
 
-              <h3 id="score" className="text-center"></h3>
+                  <div className="col-11">
+                    <div className="row align-items-center justify-content-between">
+                      <div className="col ml-3 py-2 h4 text-start">
+                        Постамат
+                      </div>
+                      <div
+                        id="score"
+                        className="col-7 mr-3 py-2 h4 text-end"
+                      ></div>
+                    </div>
+                  </div>
+                </div>
 
-              <div className="col-auto">
-                <MyMap onCalculate={onCalculate} />
+                <div className="col-6 full-height">
+                  <MyMap onCalculate={onCalculate} />
+                </div>
               </div>
             </div>
           </div>
